454b9a2a7f12fb7a51eab5744fbc2297101413c1
aaperezon <aaron-po@hotmail.co.uk>
2022-07-24 18:09:57 -0700

    update

--- js/videogallery.js
+++ js/videogallery.js
@@ -1,9 +1,13 @@
 let videogallery = () => {
     console.log("hello from videogallery.js ")
-
     var video = document.querySelectorAll('video');
     video.forEach(play => play.addEventListener('click',() =>{
         play.classList.toggle('active');
+        if(play.classList.contains('active')){
+            play.setAttribute("controls","")
+        }else{
+            play.removeAttribute("controls")
+        }
         if(play.paused){
             play.play();
         }else{
