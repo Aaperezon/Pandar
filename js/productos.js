3414bdb7ee2d4e4abfb67e918527c1493b2854e5
aaperezon <aaron-po@hotmail.co.uk>
2022-07-26 10:23:03 -0700

    needs to fix productos.css

---
+++ js/productos.js
@@ -0,0 +1,5 @@
+let productos = () => {
+    console.log("hello from productos.js")
+
+}
+window.addEventListener("load", productos());
--- js/script.js
+++ js/script.js
@@ -88,6 +88,30 @@ let init = () =>{
     //END of historias
 
 
+    //LOAD productos dinamically
+    fetch('./productos.html')
+    .then(response=> response.text())
+    .then(text=> {
+        document.getElementById('productos').innerHTML = text
+        if(navigation.classList.contains('active')){
+            for(var i  = 0; i < main_container.length; i++){
+                main_container[i].setAttribute("style","left: 20vw;")
+            }       
+        }
+        document.getElementById('productos').setAttribute("style","display:none;")
+        let new_script = document.createElement("script")
+        new_script.src = "./js/productos.js"
+        document.body.appendChild(new_script)
+    });
+    const btnProductos = document.getElementById('btnProductos');
+    btnProductos.addEventListener("click",() => {
+        hide_all()
+        document.getElementById('index').setAttribute("style","display:none;")
+        document.getElementById('productos').setAttribute("style","display:block;")
+    })
+    //END of productos
+
+
     //LOAD faqs dinamically
     fetch('./faqs.html')
     .then(response=> response.text())
